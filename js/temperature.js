dfdeb1a789652635b60c1519e9590ed4b59a6c22
HSAGGA187 <hsagga@calstatela.edu>
2019-05-08 14:12:49 -0700

    update

--- js/temperature.js
+++ js/temperature.js
@@ -1,26 +1,33 @@
-function convert() {
-    var temp = document.getElementById("temp");
-    var value = temp.value;
-  
-    if (value == "c") {
-      var convertedF = cToF();
-  
-      return document.getElementById("result").value = convertedF + "F";
-    } else if (value == "f") {
-      var convertedC = fToC();
-  
-      return document.getElementById("result").value = convertedC + "C";
-    }
-  }
-  
-  // celcius to rest
-  function cToF() {
-    var c = document.getElementById("input").value;
-    return (c * 9 / 5) + 32;
-  }
-  
-  // fahrenheit to rest
-  function fToC() {
-    var f = document.getElementById("input").value;
-    return (f - 32) * 5 / 9;
-  }
+function login() {
+    var usr = document.getElementById("username");
+    var pwd = document.getElementById("password");
+
+    return document.getElementById("result").value = usr.value + pwd.value;
+}
+
+// function convert() {
+//     var temp = document.getElementById("temp");
+//     var value = temp.value;
+
+//     if (value == "c") {
+//       var convertedF = cToF();
+
+//       return document.getElementById("result").value = convertedF + "F";
+//     } else if (value == "f") {
+//       var convertedC = fToC();
+
+//       return document.getElementById("result").value = convertedC + "C";
+//     }
+//   }
+
+//   // celcius to rest
+//   function cToF() {
+//     var c = document.getElementById("input").value;
+//     return (c * 9 / 5) + 32;
+//   }
+
+//   // fahrenheit to rest
+//   function fToC() {
+//     var f = document.getElementById("input").value;
+//     return (f - 32) * 5 / 9;
+//   }
