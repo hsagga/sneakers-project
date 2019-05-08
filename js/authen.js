cc092e0f55f4073959ef3b2dd944ffeefda8f81a
HSAGGA187 <hsagga@calstatela.edu>
2019-05-08 15:18:52 -0700

    validate login

--- js/authen.js
+++ js/authen.js
@@ -1,14 +1,25 @@
+var attempt = 3;
+
 function login() {
-    var error = "user cannot be blank!"
-    var usr = document.getElementById("username");
-    var pwd = document.getElementById("password");
+    var usr = document.getElementById("username").value;
+    var pwd = document.getElementById("password").value;
 
     // security check 
-    if (usr.value == " " | usr.value == null ) {
-        return document.getElementById("result").value = error;
+    // loop thro database looking for the user !!!
+    if (usr == "blah" && pwd == "123") {
+        alert("Login Successfully");
+        window.location = "success.html";
+        return false;
+    } else {
+        attempt--;
+        alert("you have left " + attempt + " attempts");
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
     }
-
-    // loop thro database looking for the user
 
     // return default
     return document.getElementById("result").value = usr.value + pwd.value;
